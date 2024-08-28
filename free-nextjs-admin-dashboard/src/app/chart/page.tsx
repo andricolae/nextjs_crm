11191b6489f48e73cc6bfcfcc287534406c02fa7
Elisei-26 <pralaelisei76@gmail.com>
2024-08-28 19:41:51 +0300

    Fix indentation, add Clients and Company info options in Sidebar

--- free-nextjs-admin-dashboard/src/app/chart/page.tsx
+++ free-nextjs-admin-dashboard/src/app/chart/page.tsx
@@ -4,17 +4,17 @@ import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "Next.js Chart | TailAdmin - Next.js Dashboard Template",
-  description:
-    "This is Next.js Chart page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+    title: "Next.js Chart | TailAdmin - Next.js Dashboard Template",
+    description:
+        "This is Next.js Chart page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
 };
 
 const BasicChartPage: React.FC = () => {
-  return (
-    <DefaultLayout>
-      <Chart />
-    </DefaultLayout>
-  );
+    return (
+        <DefaultLayout>
+            <Chart />
+        </DefaultLayout>
+    );
 };
 
 export default BasicChartPage;
--- free-nextjs-admin-dashboard/src/app/forms/form-elements/page.tsx
+++ free-nextjs-admin-dashboard/src/app/forms/form-elements/page.tsx
@@ -4,17 +4,17 @@ import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
 export const metadata: Metadata = {
-  title: "Next.js Form Elements | TailAdmin - Next.js Dashboard Template",
-  description:
-    "This is Next.js Form Elements page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+    title: "Next.js Form Elements | TailAdmin - Next.js Dashboard Template",
+    description:
+        "This is Next.js Form Elements page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
 };
 
 const FormElementsPage = () => {
-  return (
-    <DefaultLayout>
-      <FormElements />
-    </DefaultLayout>
-  );
+    return (
+        <DefaultLayout>
+            <FormElements />
+        </DefaultLayout>
+    );
 };
 
 export default FormElementsPage;
