0858fbf0dd2a79397a2d7238858541840e088df2
Elisei-26 <pralaelisei76@gmail.com>
2024-10-04 17:48:37 +0300

    Sidebar and tables width changed

--- src/components/CompanyInfo/TableCompanyInfo.tsx
+++ src/components/CompanyInfo/TableCompanyInfo.tsx
@@ -43,7 +43,7 @@ const TableCompanyInfo = () => {
     }, []);
 
     return (
-        <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+        <div className="rounded-sm border border-stroke bg-white pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
             <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
                 Companies
             </h4>
--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -14,18 +14,20 @@ export default function DefaultLayout({
             {/* <!-- ===== Page Wrapper Start ===== --> */}
             <div className="flex">
                 {/* <!-- ===== Sidebar Start ===== --> */}
-                <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+                <div className="w-[12%]">
+                    <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+                </div>
                 {/* <!-- ===== Sidebar End ===== --> */}
 
                 {/* <!-- ===== Content Area Start ===== --> */}
-                <div className="relative flex flex-1 flex-col lg:ml-72.5">
+                <div className="relative flex flex-1 flex-col w-[88%]">
                     {/* <!-- ===== Header Start ===== --> */}
                     <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
                     {/* <!-- ===== Header End ===== --> */}
 
                     {/* <!-- ===== Main Content Start ===== --> */}
                     <main>
-                        <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                        <div className="mx-auto max-w-screen-2xl pt-8 pb-8">
                             {children}
                         </div>
                     </main>
